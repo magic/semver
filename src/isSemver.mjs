cc9679c05bd9fb840ee231d15acd29020576d9ea
Jascha <jascha@jaeh.at>
2020-01-16 06:53:51 +0100

    add isSemver

---
+++ src/isSemver.mjs
@@ -0,0 +1,17 @@
+import is from '@magic/types'
+import { parse } from './parse.mjs'
+import { serialize } from './serialize.mjs'
+
+export const isSemver = v => {
+  try {
+    if (is.string(v)) {
+      const p = parse(v)
+      return is.obj(p) && is.num(p.major)
+    } else {
+      const s = serialize(v)
+      return is.str(s) && is.num(parseInt(s.split('.')[1]))
+    }
+  } catch(_) {
+    return false
+  }
+}
