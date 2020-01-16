8276a2106cf791d3424dc89a976f0e5df123a497
Jascha <jascha@jaeh.at>
2020-01-16 07:02:25 +0100

    export bump and isSemver from index, alphabetize export keys

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,18 @@
+import { bump as b } from './bump.mjs'
+import { isSemver as is } from './isSemver.mjs'
 import { parse as p } from './parse.mjs'
 import { serialize as s } from './serialize.mjs'
 
+export const bump = b
+export const isSemver = is
 export const parse = p
 export const stringify = s
 export const serialize = s
 
 export default {
+  bump,
+  isSemver,
   parse,
-  stringify,
   serialize,
+  stringify,
 }
