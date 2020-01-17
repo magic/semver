f15bf40d0558a01291ab6fc7fc4673ab5361dad5
Jascha <jascha@jaeh.at>
2020-01-17 06:38:21 +0100

    add length comparison functions

--- src/index.mjs
+++ src/index.mjs
@@ -1,17 +1,22 @@
 import { bump as b } from './bump.mjs'
+import { isBigger as isB, isSmaller as isS } from './length.mjs'
 import { isSemver as is } from './isSemver.mjs'
 import { parse as p } from './parse.mjs'
 import { serialize as s } from './serialize.mjs'
 
 export const bump = b
+export const isBigger = isB
 export const isSemver = is
+export const isSmaller = isS
 export const parse = p
 export const stringify = s
 export const serialize = s
 
 export default {
   bump,
+  isBigger,
   isSemver,
+  isSmaller,
   parse,
   serialize,
   stringify,
---
+++ src/length.mjs
@@ -0,0 +1,52 @@
+import is from '@magic/types'
+
+import { parse } from './parse.mjs'
+
+export const isBigger = (a, b) => {
+  if (a === b) {
+    return false
+  }
+
+  const pA = parse(a)
+  const pB = parse(b)
+
+  if (pA.major > pB.major) {
+    return true
+  }
+
+  if (pA.major === pB.major) {
+    if (pA.minor > pB.minor) {
+      return true
+    }
+  }
+
+  if (pA.minor === pB.minor) {
+    if (pA.patch > pB.patch) {
+      return true
+    }
+  }
+
+  if (pA.patch === pB.patch) {
+    if (is.empty(pA.demo) && !is.empty(pB.demo)) {
+      return true
+    }
+
+    if (!is.empty(pA.demo) && is.empty(pB.demo)) {
+      return false
+    }
+
+    if (pA.demo.string === 'beta' && pB.demo.string === 'alpha') {
+      return true
+    }
+
+    if (pA.demo.string === pB.demo.string) {
+      if (pA.demo.version > pB.demo.version) {
+        return true
+      }
+    }
+  }
+
+  return false
+}
+
+export const isSmaller = (a, b) => !isBigger(a, b)
