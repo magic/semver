df6700164b35a294d164da2a47a74145ab072a95
jascha <j@jaeh.at>
2021-12-24 17:17:15 +0100

    test: use @magic/test.version v2

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,19 +1,16 @@
-import { is, version } from '@magic/test'
+import { version } from '@magic/test'
 
 import semver from '../src/index.mjs'
 
 const spec = {
-  parse: is.fn,
-  stringify: is.fn,
-  serialize: is.fn,
-  stringify: is.fn,
-  bump: is.fn,
-  isSemver: is.fn,
-  isBigger: is.fn,
-  isSmaller: is.fn,
+  parse: 'fn',
+  stringify: 'fn',
+  serialize: 'fn',
+  stringify: 'fn',
+  bump: 'fn',
+  isSemver: 'fn',
+  isBigger: 'fn',
+  isSmaller: 'fn',
 }
 
-export default [
-  { fn: () => version.lib(semver, spec), expect: is.empty },
-  { fn: () => version.spec(spec, semver), expect: is.empty },
-]
+export default version(semver, spec)
