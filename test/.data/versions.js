770abe2df6a4e51a89d159633292f368fe8b5c59
Jascha Ehrenreich <jascha@artificialmuseum.com>
2025-12-13 03:54:41 +0100

    more test versions

--- test/.data/versions.js
+++ test/.data/versions.js
@@ -1,6 +1,40 @@
 export const versions = {
   'v0.0.1': { major: 0, minor: 0, patch: 1, v: 'v0.0.1' },
-  'v1.0.0': { major: 1, minor: 0, patch: 0, v: 'v1.0.0' },
+  'v2.3.1': { major: 2, minor: 3, patch: 1, v: 'v2.3.1' },
+  'v9.9.9': { major: 9, minor: 9, patch: 9, v: 'v9.9.9' },
+  'v1.2.3': { major: 1, minor: 2, patch: 3, v: 'v1.2.3' },
+  'v5.44.33': { major: 5, minor: 44, patch: 33, v: 'v5.44.33' },
+  'v11.22.33': { major: 11, minor: 22, patch: 33, v: 'v11.22.33' },
+  'v111.222.333': { major: 111, minor: 222, patch: 333, v: 'v111.222.333' },
+  'v111.222.333-alpha.1': {
+    major: 111,
+    minor: 222,
+    patch: 333,
+    demo: { string: 'alpha', version: 1 },
+    v: 'v111.222.333-alpha.1',
+  },
+  'v111.222.333-beta.1': {
+    major: 111,
+    minor: 222,
+    patch: 333,
+    demo: { string: 'beta', version: 1 },
+    v: 'v111.222.333-beta.1',
+  },
+  'v111.222.333-charlie.1': {
+    major: 111,
+    minor: 222,
+    patch: 333,
+    demo: { string: 'charlie', version: 1 },
+    v: 'v111.222.333-charlie.1',
+  },
+  'v111.222.333-gamma.1': {
+    major: 111,
+    minor: 222,
+    patch: 333,
+    demo: { string: 'gamma', version: 1 },
+    v: 'v111.222.333-gamma.1',
+  },
+
   '0.0.1': { major: 0, minor: 0, patch: 1, v: '0.0.1' },
   '1.0.0': { major: 1, minor: 0, patch: 0, v: '1.0.0' },
   '0.23.5-alpha.12': {
